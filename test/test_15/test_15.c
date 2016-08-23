d6d8af32f1cc8d2419000c0f47ba877b7e018753
alick97 <alick97@outlook.com>
2016-08-24 01:12:37 +0800

    strncat strstr经典实现 二维有序矩阵查找 杨辉矩阵

--- test/test_15/test_15.c
+++ test/test_15/test_15.c
@@ -26,36 +26,70 @@ void test_my_strncat()
 }
 
 //2.模拟实现strstr
-char * my_strstr(const char *dest, const char *src)
-{
-	assert(dest && src);
-	const char* p_dest = dest;
-	const char* p_src = src;
-	const char* p_ret = dest;
-	while (*p_ret)
-	{
-		p_dest = p_ret;
-		while (*p_dest++ == *p_src++)
-		{
-			if (*p_src == '\0')
-			{
-				return (char*)p_ret;
-			}
-		}
+//char * my_strstr(const char *dest, const char *src)
+//{
+//	assert(dest && src);
+//	const char* p_dest = dest;
+//	const char* p_src = src;
+//	const char* p_ret = dest;
+//	while (*p_ret)
+//	{
+//		p_dest = p_ret;
+//		while (*p_dest++ == *p_src++)
+//		{
+//			if (*p_src == '\0')
+//			{
+//				return (char*)p_ret;
+//			}
+//		}
+//
+//		p_ret++;
+//		p_src = src;
+//	}
+//
+//	return NULL;
+//}
+//
+//
 
-		p_ret++;
-		p_src = src;
+// 常用经典 实现方法
+char *my_strstr(const char* str1, const char *str2)
+{
+	char *cp = (char*)str1;
+	char *s1,*s2;
+	
+	if (!*str2) // 这个if 不多余 对于"" "" 的处理 不是NULL  而是s1""的地址
+	{
+		return ((char*) str1);
 	}
 
+	while (*cp)
+	{
+		s1 = cp;
+		s2 = (char*) str2;
+
+		while (*s1 && *s2 && !(*s1-*s2))
+		{
+			s1++;
+			s2++;
+		}
+
+		if (!*s2)
+		{
+			return cp;
+		}
+		cp++;
+	}
 	return NULL;
 }
+
 
 void test_strstr()
 {
 	char str1[] = "nihao sunshine hello";
 	char str2[] = " sunshine hello";
 	char str3[] = "nihao sun note";
-	
+	char str4[] = "";	
 	
 	char *ret = my_strstr(str1, str2);
 	if (ret)
@@ -67,6 +101,29 @@ void test_strstr()
 		printf("not found \n");
 	}
 	ret = my_strstr(str1, str3);
+
+	if (ret)
+	{
+		printf("%s\n", ret);
+	}
+	else
+	{
+		printf("not found \n");
+	}
+	ret = my_strstr(str1, str4);
+
+	if (ret)
+	{
+		printf("%s\n", ret);
+	}
+	else
+	{
+		printf("not found \n");
+	}
+
+	char str_1[] = "";
+	char str_2[] = "";
+	ret = my_strstr(str_1, str_2);
 
 	if (ret)
 	{
@@ -91,17 +148,17 @@ void test_strstr()
 //查找数字7.
 //
 
-
+//   从 右上角  到 左下角 搜索
 int search_in_matrix(int arr[][3], int x, int y, int target)
 {
 	int x_pos = 0;
-	while (x_pos < x && y >= 0)
+	while (x_pos < x && y > 0)
 	{
-		if (target == arr[x_pos][y])
+		if (target == arr[x_pos][y-1])
 		{
-			return 1;
+			return 1; // success
 		}
-		else if (target < arr[x_pos][y])
+		else if (target < arr[x_pos][y-1])
 		{
 			y--;
 		}
@@ -113,14 +170,17 @@ int search_in_matrix(int arr[][3], int x, int y, int target)
 	return 0;
 }
 
-void test()
+void test_search()
 {
-	int arr[][3] = {{1,2,3},{4,5,6},{7,8,9} ;
+	int arr[][3] = {{1,2,3},{4,5,6},{7,8,9}};
+		printf("find 7 : %d\n", search_in_matrix(arr, 3,3, 7));
+		printf("find -1 : %d\n", search_in_matrix(arr, 3,3, -1));
 }
 
 int main()
 {
 	//test_my_strncat();
 	test_strstr();
+	//test_search();
 	return 0;
 }
