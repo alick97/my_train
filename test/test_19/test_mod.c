d0787de629242ca8c483d8dd9fac81917cc7aec4
alick97 <alick97@outlook.com>
2016-08-28 23:58:06 +0800

    可变参数列表 模拟printf函数  模除的各种情况测试结果

---
+++ test/test_19/test_mod.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+	printf("7  %  2 = %d\n",7%2 );
+	printf("-7 %  2 = %d\n ",-7%2 );
+	printf("-7 % -2 = %d\n",-7%-2 );
+	printf("7 % -2  = %d\n",7%-2 );
+	return 0;
+}
