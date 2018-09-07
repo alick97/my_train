e0fc43ae83d23b889cafb8cfe7e75ae7a50b9076
alick <alick97@outlook.com>
2018-09-07 22:20:28 +0800

    add o(n) soulution lengthOfLongestSubstring2

--- leetcode/problems/3_longest_substring_without_reprating_characters/solution.cpp
+++ leetcode/problems/3_longest_substring_without_reprating_characters/solution.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s);
+    int lengrhOfLongestSubstring2(string s);
 };
 
 int Solution::lengthOfLongestSubstring(string s) {
@@ -35,6 +36,25 @@ int Solution::lengthOfLongestSubstring(string s) {
     return max_result;
 }
 
+int Solution::lengrhOfLongestSubstring2(string s) {
+    // method O(n)
+    int start = -1;
+    int max_length = 0;
+    int ch_index[256];
+    for(int i = 0; i < 256; i++) {
+        ch_index[i] = -1;
+    }
+
+    for(int index=0; index < s.length(); ++index) {
+        if (ch_index[static_cast<unsigned int>(s[index])] > start) {
+            start = ch_index[static_cast<unsigned int>(s[index])];
+        }
+        ch_index[static_cast<unsigned int>(s[index])] = index;
+        max_length = max(max_length, index - start);
+    }
+    return max_length;
+}
+
 int main() {
     Solution s;
     // abcabcbb // 3
@@ -44,5 +64,11 @@ int main() {
     // pwwkew // 3
     cout << s.lengthOfLongestSubstring("pwwkew") << endl;
 
+    // abcabcbb // 3
+    cout << s.lengrhOfLongestSubstring2("abcabcbb") << endl;
+    // bbbbb // 1
+    cout << s.lengrhOfLongestSubstring2("bbbbb") << endl;
+    // pwwkew // 3
+    cout << s.lengrhOfLongestSubstring2("pwwkew") << endl;
     return 0;
 }
