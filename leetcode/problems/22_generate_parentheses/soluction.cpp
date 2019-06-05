cc31d22dc74c13fde932ab659af9b63762985221
alick <alick97@outlook.com>
2019-06-06 00:12:41 +0800

    leetcode: update soluction algorithm for more speed and less memory

--- leetcode/problems/22_generate_parentheses/soluction.cpp
+++ leetcode/problems/22_generate_parentheses/soluction.cpp
@@ -4,51 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    bool is_ok(const string &s) {
-        int count = 0;
-        for (size_t i = 0; i < s.size(); i++) {
-            if (s[i] == '(') {
-                count += 1;
-            } else {
-                count -=1;
-            }
-            if (count < 0) {
-                return false;
-            }
-        }
-
-        if (count == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     vector<string> generateParenthesis(int n) {
-        vector<string> v1, v2;
-        if (n < 0) {
-            return v1;
+        vector<string> v;
+        if (n == 0) {
+            v.push_back("");
+            return v;
         }
-        v1.push_back("(");
-        v1.push_back(")");
-        for (int i = 1; i < n * 2; i++) {
-            while (!v1.empty()) {
-                string s = v1.at(v1.size() -1);
-                v1.pop_back();
 
-                v2.push_back(s + ")");
-                v2.push_back(s + "(");
-            }
-            v1.swap(v2);
-        }
-        while (!v1.empty()) {
-            string s = v1.at(v1.size() -1);
-            v1.pop_back();
-            if (is_ok(s)) {
-                v2.push_back(s);
+        for (int left_count = 0; left_count < n; left_count++) {
+            vector<string> left_str_v = generateParenthesis(left_count);
+            vector<string> right_str_v = generateParenthesis(n - 1 - left_count);
+            for (int i = 0; i < left_str_v.size(); i++) {
+                for (int j = 0; j < right_str_v.size(); j++) {
+                    // note: "(" + left_str_v[i] + right_str_v[j] + ")" will repeat
+                    v.push_back(left_str_v[i] + "(" + right_str_v[j] + ")");
+                }
             }
         }
-        return v2;
+        return v;
     }
 };
 
@@ -64,7 +37,7 @@ void test() {
   ]
 */
    Solution s;
-   vector<string> v = s.generateParenthesis(3);
+   vector<string> v = s.generateParenthesis(10);
    for (int i = 0; i < v.size(); i++) {
        cout << v[i] << endl;
    }
