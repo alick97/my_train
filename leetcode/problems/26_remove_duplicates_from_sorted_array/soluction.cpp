41c0340014219d86899692eb3a42db6baa482bd7
alick <alick97@outlook.com>
2019-06-11 22:28:35 +0800

    leetcode: modify algorithm, reduce circle

--- leetcode/problems/26_remove_duplicates_from_sorted_array/soluction.cpp
+++ leetcode/problems/26_remove_duplicates_from_sorted_array/soluction.cpp
@@ -5,28 +5,16 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int index_1 = 0, index_2 = 1;
-        // if (nums.size() == 0) {
-        //     return 0;
-        // }
+        int index_1 = 1, index_2 = 1;
 
-        while (index_1 < nums.size() && index_2 < nums.size()) {
-            while (index_2 < nums.size() && nums[index_1] == nums[index_2]) {
-                ++index_2;
+        if (nums.size() < 2) return nums.size();
+
+        for (; index_2 < nums.size(); ++index_2) {
+            if( nums[index_2 - 1] != nums[index_2]) {
+                nums[index_1++] = nums[index_2];
             }
-
-            if (index_2 < nums.size()) {
-                if (index_2 - index_1 > 1) {
-                    swap(nums[index_1 + 1], nums[index_2]);
-                }
-                ++index_1;
-                ++index_2;
-            } else {
-                break;
-            }               
         }
-
-        return nums.size() == 0 ? 0 : index_1 + 1;
+        return index_1;
     }
 };
 
@@ -34,8 +22,8 @@ void test() {
 /*
 [0,0,1,1,1,2,2,3,3,4] -> 5
 */
-    // vector<int> v{0,0,1,1,1,2,2,3,3,4};
-    vector<int> v{1, 1, 2};
+    vector<int> v{0,0,1,1,1,2,2,3,3,4};
+    // vector<int> v{1, 1, 2};
     Solution s;
     int count = s.removeDuplicates(v);
     cout << "count: " << count << endl;
